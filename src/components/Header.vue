5d07c1f402d893f110ba0f5312c4e0d0aebe6634
smboolean47 <smboolean47@madrigali.dev>
2021-12-13 11:57:05 +0100

    add base layut

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/img/la-molisana-logo.png" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
